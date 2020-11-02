1eadfffc2f0f1f08a3138553a77cd940477d279b
felix <fsafaridis@hotmail.com>
2020-11-02 20:57:05 +0200

    modify aspect

--- aspectj/Aspect.java
+++ aspectj/Aspect.java
@@ -2,17 +2,17 @@
 
 aspect Test {
     
-    private volatile boolean Tree.mutex = true;
+    private volatile boolean Tree.mutex = true; // This is not enough, but its ok for the demo (should use AtomicBoolean instead)
 
     pointcut insertChange(Tree t, int a): target(t) && call(public void Tree.insert(int)) && args(a);
     
     pointcut removeChange(Tree t, int a): target(t) && call(public void Tree.remove(int)) && args(a);
 
-    void around(Tree t, int a): insertChange(t, a) {
-        if (t.mutex) {
-            t.mutex = false;
+    void around(Tree t, int a): insertChange(t, a) { 
+        if (t.mutex) {   // t.mutex.compareAndSet(true, false)
+            t.mutex = false; // skipped step in case of AtomicBoolean implementation
             proceed(t, a);
-            t.mutex = true;
+            t.mutex = true; // t.mutex.set(true)
         } else {
             t.insert(a);
         }
